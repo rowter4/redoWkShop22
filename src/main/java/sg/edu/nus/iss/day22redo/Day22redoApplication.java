cce81c46e9cb0bb41903d8bce92e85064ec15ed6
rowter4 <nadzir.4@gmail.com>
2022-05-06 12:52:15 +0800

    intial commit

---
+++ src/main/java/sg/edu/nus/iss/day22redo/Day22redoApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day22redo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day22redoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day22redoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/day22redo/Day22redoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day22redo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day22redoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
